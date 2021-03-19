ea9f73aeb626fad9121bffe19728b8ee31f79cc6
HelixFix <jerome.rinner@gmail.com>
2021-03-19 10:40:31 +0100

    :construction: Change Copyright name

--- index.php
+++ index.php
@@ -195,7 +195,7 @@ $test = $tblWebinaire2->fetch();
             <!-- Copyright -->
             <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
                 © 2021 Copyright:
-                <a class="text-dark" href="http://www.serfa.fr/">Serfa</a>
+                <a class="text-dark" href=#">Web Service</a>
             </div>
             <!-- Copyright -->
         </footer>
