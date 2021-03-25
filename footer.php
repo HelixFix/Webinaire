7d2b862df2c7052a5006c4397bdcf70392e10861
HelixFix <jerome.rinner@gmail.com>
2021-03-25 15:35:36 +0100

    :art: Split the code

---
+++ footer.php
@@ -0,0 +1,65 @@
+<footer>
+        <footer class="text-center text-lg-start">
+            <!-- Grid container -->
+            <div class="container p-4">
+                <!--Grid row-->
+                <div class="row">
+                    <!--Grid column-->
+                    <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                        <h5>Où nous trouver ?</h5>
+
+                        <span>
+                            <b>
+                                SITE SCHLUMBERGER - Siège social
+                            </b>
+                        </span>
+
+                        <p>
+                            4 rue Schlumberger 68200 Mulhouse
+                        </p>
+
+                        <span>
+                            <b>
+                                SITE KENNEDY
+                            </b>
+                        </span>
+
+                        <p>
+                            22 avenue Kennedy 68200 Mulhouse
+                        </p>
+
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                        <h5>Contact </h5>
+
+                        <p>
+                            Sabrina Tedjiza, Assistante de formation
+                            <br>
+                            s.tedjiza@praxis.alsace
+                            <br>
+                            03 89 60 67 85
+                        </p>
+                    </div>
+                    <!--Grid column-->
+                </div>
+                <!--Grid row-->
+            </div>
+            <!-- Grid container -->
+
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                © 2021 Copyright:
+                <a class="text-dark" href=#">Web Service</a>
+                SIRET 778 952 176 00023 Code APE 8559B
+            </div>
+            <!-- Copyright -->
+        </footer>
+    </footer>
+</div>
+<!-- MDB -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.js"></script>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,47 @@
+<div class="container">
+
+    <header>
+
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="images/ESPS-58x58.jpg" alt=""></a>
+                <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <!-- <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li> -->
+                        <li class="btn" id="connect">
+                            <a class="nav-link" href="login.html" tabindex="-1" aria-disabled="" style="padding-left: 32px; padding-right: 32px;">Se connecter</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <br>
+        <!-- Background image -->
+        <div class="p-5 text-center bg-image">
+            <div class=" mask" style="background-color: rgba(0, 0, 0, 0.6)">
+                <div class="d-flex justify-content-center align-items-center h-100">
+                    <div class="text-white">
+                        <h1 class="mb-3">Journées Portes Ouvertes </h1>
+                        <!-- <h4 class="mb-3">Subheading</h4> -->
+                        <a class="btn btn-outline-light btn-lg" href="form-compte.html" role="button">S'inscrire</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Background image -->
+        <br>
+
+
+
+    </header>
