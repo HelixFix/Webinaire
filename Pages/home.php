eba347485b57fd41152953e0b2ec5784505d5453
HelixFix <jerome.rinner@gmail.com>
2021-03-31 16:23:44 +0200

    :art: Format the code

--- Pages/home.php
+++ Pages/home.php
@@ -24,8 +24,8 @@
 
 
     <div class="row">
-        <?php
 
+        <?php
         while ($ligne = $tblWebinaire->fetch()) {
             // var_dump($ligne)
             $date = utf8_encode(strftime("%A %d %B %G", strtotime($ligne['date'])));
@@ -33,37 +33,47 @@
 
 
             <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+
                 <?php if (isset($_SESSION['user']['nom'])) { ?>
+
+                    <!-- Supression -->
                     <a href="index.php?page=formwebinaire&idwebinaire=<?php echo $ligne['id']; ?>" class="btn btnhome"><img class="btn-supprimer" src="images\x-supp.png" alt="supprimer"></a>
+
+                    <!-- Edition -->
                     <a href="index.php?page=formwebinaire&idwebinaire=<?php echo $ligne['id']; ?>" class="btn btnhome"><img class="btn-editer" src="images\edition.png" alt="editer"></a>
                 <?php } ?>
+
                 <div class="card">
                     <img class="card-img-top" src="https://loremflickr.com/640/360" alt="Card image cap">
+
                     <div class="card-body">
                         <p class=""><small class="text-muted">Diffusé le <?php echo $date ?></small></p>
+
                         <h3 class="card-title"><?php echo $ligne['nomLive'] ?></h3>
+
                         <p class="card-text clamp" onclick="(function(){classList.toggle('clamp')})()" ?><?php echo $ligne['resumer'] ?>
                         </p>
-
 
                         <a href="index.php?page=formwebinaire&idwebinaire=<?php echo $ligne['id']; ?>" class="btn btnHome">Rejoindre le webinaire</a>
                     </div>
                 </div>
             </div>
-
-            
         <?php } ?>
+
+        <!-- Ajout -->
         <?php if (isset($_SESSION['user']['nom'])) { ?>
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-          <a href="#" class="btn btnhome"><img class="btn-supprimer" src="images\x-supp.png" alt="supprimer"></a>
-          <a href="#" class="btn btnhome"><img class="btn-editer" src="images\edition.png" alt="editer"></a>
-          <div class="ajouter-webinaire">
-            <a href="form-edit.html" class="btn btnhome"><img class="btn-ajouter" src="images\add.png" alt="ajouter un webiniare"></a>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                <a href="#" class="btn btnhome"><img class="btn-supprimer" src="images\x-supp.png" alt="supprimer"></a>
 
-          </div>
-          <?php } ?>
-        <?php if (isset($_SESSION['user']['nom'])) echo "Ma variable de session " . $_SESSION['user']['nom']; ?>
+                <a href="#" class="btn btnhome"><img class="btn-editer" src="images\edition.png" alt="editer"></a>
 
-    </div>
-    <br>
+                <div class="ajouter-webinaire">
+                    <a href="form-edit.html" class="btn btnhome"><img class="btn-ajouter" src="images\add.png" alt="ajouter un webiniare"></a>
+                </div>
+            <?php } ?>
+
+            <?php if (isset($_SESSION['user']['nom'])) echo "Ma variable de session " . $_SESSION['user']['nom']; ?>
+
+            </div>
+            <br>
 </body>
