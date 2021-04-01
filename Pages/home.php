4cc0f2cb420ff2902fda385e52bb9bf717a936ec
HelixFix <jerome.rinner@gmail.com>
2021-04-01 15:01:23 +0200

    :art: Enhance Ajout card

--- Pages/home.php
+++ Pages/home.php
@@ -29,10 +29,6 @@
 
         <?php if (isset($_SESSION['user']['nom'])) { ?>
             <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                <a href="#" class="btn btnhome"><img class="btn-supprimer" src="images\x-supp.png" alt="supprimer"></a>
-
-                <a href="#" class="btn btnhome"><img class="btn-editer" src="images\edition.png" alt="editer"></a>
-
                 <div class="ajouter-webinaire">
                     <a href="index.php?page=form-edit" class="btn btnhome"><img class="btn-ajouter" src="images\add.png" alt="ajouter un webiniare"></a>
                 </div>
