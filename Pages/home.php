e7f6b7b981cebeb9e8e6ea171eb1116605e65228
HelixFix <jerome.rinner@gmail.com>
2021-04-01 10:59:06 +0200

    :bug: Fix cards display

--- Pages/home.php
+++ Pages/home.php
@@ -36,46 +36,47 @@
                 <div class="ajouter-webinaire">
                     <a href="index.php?page=form-edit" class="btn btnhome"><img class="btn-ajouter" src="images\add.png" alt="ajouter un webiniare"></a>
                 </div>
-            <?php } ?>
-
             </div>
-
-            <?php
-            while ($ligne = $tblWebinaire->fetch()) {
-                // var_dump($ligne)
-                $date = utf8_encode(strftime("%A %d %B %G", strtotime($ligne['date'])));
-            ?>
+        <?php } ?>
 
 
-                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
 
-                    <?php if (isset($_SESSION['user']['nom'])) { ?>
+        <?php
+        while ($ligne = $tblWebinaire->fetch()) {
+            // var_dump($ligne)
+            $date = utf8_encode(strftime("%A %d %B %G", strtotime($ligne['date'])));
+        ?>
 
-                        <!-- Supression -->
-                        <a href="index.php?page=form-edit&idwebinaire=<?php echo $ligne['id']; ?>" class="btn btnhome"><img class="btn-supprimer" src="images\x-supp.png" alt="supprimer"></a>
 
-                        <!-- Edition -->
-                        <a href="index.php?page=form-edit&idwebinaire=<?php echo $ligne['id']; ?>" class="btn btnhome"><img class="btn-editer" src="images\edition.png" alt="editer"></a>
-                    <?php } ?>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12">
 
-                    <div class="card">
-                        <img class="card-img-top" src="https://loremflickr.com/640/360" alt="Card image cap">
+                <?php if (isset($_SESSION['user']['nom'])) { ?>
 
-                        <div class="card-body">
-                            <p class=""><small class="text-muted">Diffusé le <?php echo $date ?></small></p>
+                    <!-- Supression -->
+                    <a href="index.php?page=form-edit&idwebinaire=<?php echo $ligne['id']; ?>" class="btn btnhome"><img class="btn-supprimer" src="images\x-supp.png" alt="supprimer"></a>
 
-                            <h3 class="card-title"><?php echo $ligne['nomLive'] ?></h3>
+                    <!-- Edition -->
+                    <a href="index.php?page=form-edit&idwebinaire=<?php echo $ligne['id']; ?>" class="btn btnhome"><img class="btn-editer" src="images\edition.png" alt="editer"></a>
+                <?php } ?>
 
-                            <p class="card-text clamp" onclick="(function(){classList.toggle('clamp')})()" ?><?php echo $ligne['resumer'] ?>
-                            </p>
+                <div class="card">
+                    <img class="card-img-top" src="https://loremflickr.com/640/360" alt="Card image cap">
 
-                            <a href="index.php?page=formwebinaire&idwebinaire=<?php echo $ligne['id']; ?>" class="btn btnHome">Rejoindre le webinaire</a>
-                        </div>
+                    <div class="card-body">
+                        <p class=""><small class="text-muted">Diffusé le <?php echo $date ?></small></p>
+
+                        <h3 class="card-title"><?php echo $ligne['nomLive'] ?></h3>
+
+                        <p class="card-text clamp" onclick="(function(){classList.toggle('clamp')})()" ?><?php echo $ligne['resumer'] ?>
+                        </p>
+
+                        <a href="index.php?page=formwebinaire&idwebinaire=<?php echo $ligne['id']; ?>" class="btn btnHome">Rejoindre le webinaire</a>
                     </div>
                 </div>
-            <?php } ?>
+            </div>
+        <?php } ?>
 
-            <!-- Ajout -->
+        <!-- Ajout -->
 
-            <br>
+        <br>
 </body>
